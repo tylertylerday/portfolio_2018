e113ba134b999f77ca61dfb481d1c1bd89e3cc0c
Tyler Day <tylerday89@gmail.com>
2018-10-11 11:28:24 -0700

    updated from desktop

--- contact.php
+++ contact.php
@@ -36,7 +36,7 @@ if(get_magic_quotes_gpc()) {
 // Enter the email address that you want to emails to be sent to.
 // Example $address = "joe.doe@yourdomain.com";
 
-//$address = "example@themeforest.net";
+//$address = "example@email.net";
 $address = "tylerday89@gmail.com";
 
 
